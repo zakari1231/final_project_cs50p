2f291161908e567aad272c693f3fe2bd44619ed4
zakari1231 <zakari_1231@hotmail.fr>
2022-06-25 14:06:40 +0100

    change the function name

--- class_helper.py
+++ class_helper.py
@@ -419,7 +419,7 @@ class Crud_db:
             except ValueError:
                 sys.exit('value Error')
 
-    def save_to_html_pdf(self):
+    def save_the_last_bill_to_html_pdf(self):
         env = Environment(loader=FileSystemLoader('templates'))
         # 3. Load the template from the Environment
         template = env.get_template('th_bill.html')
