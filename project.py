c848f498e7e733f397e842ff0afda575503ca3be
zakari1231 <zakari_1231@hotmail.fr>
2022-06-27 15:04:48 +0100

    add more detail in the help

--- project.py
+++ project.py
@@ -9,7 +9,8 @@ def print_help():
     # TODO fix the ortoghraph error
     help_string = ''' 
     this is a Seller Management Systeme that contain many featsers allow the seller track his revenue and expenses and
-    register his products with the price and the margin, and register all of this informations in a database that allow to Create Read Update Delete all the information to manage his selling and expense.
+    register his products with the price and the margin, and register all of this informations in a database that allow 
+    to Create Read Update Delete all the information to manage his selling and expense.
 
      -*- to get help with this project you just type help like this "python project.py help"
         that will show you all the command line allwoed in this project.
@@ -23,6 +24,18 @@ def print_help():
 
      -*- to create all table in the database use "python project.py create_tables"
         this commande create all the nesessairy table in the database.
+
+     -*- to create new bill for a client use "python project.py create_new_bill"
+        this command will create a new bill fro a client all you have to do is to enter the name of the client 
+        and the id of the product he buy the number of this product, after that if he buy other product just type yes and
+        continue entering what he buy.
+
+     -*- to create a pdf bill for this client use " python project.py create_pdf_bill"
+        this command will create a pdf file and an html file for the last bill you created. 
+
+     -*- to create a csv file use " python project.py save_to_csv "
+        this command will create a 3 csv file from the tables 'general bill', 'expenses', 'total'
+         * if you want to create a csv file for only a specefique day add the date in a fromat " %Y-%m-%d ". 
 
      '''
     return help_string
