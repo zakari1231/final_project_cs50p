76576b4c2161a191742de1751faa959989dea05e
zakari1231 <zakari_1231@hotmail.fr>
2022-06-27 01:07:10 +0100

    fix help fucntion

--- project.py
+++ project.py
@@ -8,7 +8,8 @@ def main():
 def print_help():
     # TODO fix the ortoghraph error
     help_string = ''' 
-    this is a Seller Management Systeme that contain many featsers allow the seller track his revenue and expenses and register his products with the price and the margin, and register all of this informations in a database that allow to Create Read Update Delete all the information to manage his selling and expense.
+    this is a Seller Management Systeme that contain many featsers allow the seller track his revenue and expenses and
+    register his products with the price and the margin, and register all of this informations in a database that allow to Create Read Update Delete all the information to manage his selling and expense.
 
      -*- to get help with this project you just type help like this "python project.py help"
         that will show you all the command line allwoed in this project.
