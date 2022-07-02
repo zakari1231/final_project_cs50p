be0bf5894bc25f604a97bbd666a7859e655c719f
zakari1231 <zakari_1231@hotmail.fr>
2022-07-02 15:30:31 +0100

    add test login logout

--- test_project.py
+++ test_project.py
@@ -1,5 +1,7 @@
 import pytest
 import sqlite3
+import time, datetime
+from datetime import timedelta
 # from prefect.tasks.database.sqlite import SQLiteQuery
 
 from class_helper import Crud_db
@@ -17,6 +19,20 @@ def test_tables():
     # print(result)
     
     assert len(result) == 8
+
+def test_login_logout():
+    # test if user login the time will be this time + 3 hours if not user is logout
+    db.connect()
+    date_time_now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    time_to_logout = datetime.datetime.now() + timedelta(hours=3)
+    query = ''' SELECT max(date_time_logout) FROM login_or_not '''
+    db.execute(query)
+    result = db.cursor.fetchall()
+    if result[0][0] < date_time_now:
+        assert db.check_if_login() == False
+    else:
+        assert db.check_if_login() == True
+
 
 # # import writer
 
@@ -37,10 +53,10 @@ def test_tables():
 # #     assert db.save_to_csv() == 
 
 
-class TestSQLiteQuery:
-    def test_run_with_no_db(self):
-        sqlite_query = db.connect()
+# class TestSQLiteQuery:
+#     def test_run_with_no_db(self):
+#         sqlite_query = db.connect()
 
-        msg_match = "The db must be specified"
-        with pytest.raises(ValueError, match=msg_match):
-            sqlite_query
+#         msg_match = "The db must be specified"
+#         with pytest.raises(ValueError, match=msg_match):
+#             sqlite_query
